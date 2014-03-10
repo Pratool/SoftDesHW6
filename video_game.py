68b1846a6d08bdbeadc204cb8f31533a74541152
gabriellee <gabrielle.ewall@students.olin.edu>
2014-03-09 23:13:57 -0400

    generally far better; bounces off walls pretty good

--- video_game.py
+++ video_game.py
@@ -31,12 +31,11 @@ class Ball:
     """ This is the ball the the user controls """
     
     def __init__(self, x, y, speed):
-        self.x = x
-        self.y = y
+        self.pos = [x, y]
         self.color = (255, 255, 255)
-        self.sprite = pygame.image.load('ball.gif')
+        self.sprite = pygame.image.load('paul.gif')
         self.rect = self.sprite.get_rect()
-        self.rect.topleft = (self.x, self.y)
+        self.rect.topleft = (int(self.pos[0]), int(self.pos[1]))
         self.speed = speed
     
 #    def update(self):
@@ -52,17 +51,20 @@ class BallFollowView:
         self.screen = screen
 
     def draw(self):
-        self.screen.fill(pygame.Color(0,0,0))
+        #self.screen.fill(pygame.Color(0,0,0))
         for ball in self.model.balls:
             self.screen.blit(ball.sprite, ball.rect)
         pygame.display.flip()
         pygame.display.update()
+        
+        
 
 class BallFollowController:
     """ This manipulates the objects of BallFollowModel """
-    def __init__(self, model):
+    def __init__(self, model, screen):
         self.model = model
         self.mouse_pos = (0, 0)
+        self.screen = screen
     
     def handle_mouse_event(self, event):
         if event.type == MOUSEBUTTONDOWN:
@@ -77,28 +79,48 @@ class BallFollowController:
     
     def set_speed(self):
         target = self.mouse_pos
-        org = self.model.balls[0].rect.topleft
+        org = self.model.balls[0].pos
         vx = (target[0] - org[0])/math.sqrt((target[0]-org[0])**2 + (target[1]-org[1])**2)
         vy = (target[1] - org[1])/math.sqrt((target[0]-org[0])**2 + (target[1]-org[1])**2)
-        self.model.balls[0].speed = [vx*10, vy*10]
+        self.model.balls[0].speed = [vx, vy]
     
     def move_ball(self):
-        self.model.balls[0].rect = self.model.balls[0].rect.move(self.model.balls[0].speed)
+        scr = self.screen
+        scr.fill((255, 105, 180), model.balls[0].rect)
         
-        if self.model.balls[0].rect.left < 0 or self.model.balls[0].rect.right+34 > self.model.width:
+#        self.model.balls[0].rect = self.model.balls[0].rect.move(self.model.balls[0].speed)
+        self.model.balls[0].pos[0] += 2*self.model.balls[0].speed[0]
+        self.model.balls[0].pos[1] += 2*self.model.balls[0].speed[1]
+        error = [self.model.balls[0].pos[0] - self.model.balls[0].rect.topleft[0], self.model.balls[0].pos[1] - self.model.balls[0].rect.topleft[1]]
+        
+        self.model.balls[0].rect = self.model.balls[0].rect.move(self.model.balls[0].speed)
+        self.model.balls[0].rect = self.model.balls[0].rect.move(error)
+        
+#        if self.model.balls[0].rect.left < 0 or self.model.balls[0].rect.right > self.model.width:
+#            self.model.balls[0].speed[0] = -self.model.balls[0].speed[0]
+#        if self.model.balls[0].rect.top < 0 or self.model.balls[0].rect.bottom > self.model.height:
+#            self.model.balls[0].speed[1] = -self.model.balls[0].speed[1]
+
+        if self.model.balls[0].pos[0] < 0 or self.model.balls[0].pos[0]+35 > self.model.width:
             self.model.balls[0].speed[0] = -self.model.balls[0].speed[0]
-        if self.model.balls[0].rect.top < 0 or self.model.balls[0].rect.right+39 > self.model.height:
+        if self.model.balls[0].pos[1] < 0 or self.model.balls[0].pos[1]+35 > self.model.height:
             self.model.balls[0].speed[1] = -self.model.balls[0].speed[1]
         
+        print self.model.height, self.model.width
+        
+        #scr.make_trail()
+#        pygame.self.screen.fill((212,175,55), model.balls[0].rect)
 #         Debugging
         print 'Target', self.mouse_pos
         print 'Speed', self.model.balls[0].speed
         print 'Position', self.model.balls[0].rect.topleft
 
+        
+
 model = BallFollowModel()
 screen = pygame.display.set_mode((model.width, model.height))
 view = BallFollowView(model, screen)
-controller = BallFollowController(model)
+controller = BallFollowController(model,screen)
 
 while True:
     for event in pygame.event.get():
@@ -109,4 +131,4 @@ while True:
         controller.move_ball()
         print 'Have not clicked to stop'
     view.draw()
-    time.sleep(0.1)
+    time.sleep(0.0001)
